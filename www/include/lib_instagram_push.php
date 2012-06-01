093560519d4433ab33eea9b28020d7c39ef3ea5d
straup <straup@gmail.com>
2012-06-01 03:44:51 +0200

    more stubbing out of push functions

--- www/include/lib_instagram_push.php
+++ www/include/lib_instagram_push.php
@@ -1,5 +1,7 @@
 <?php
 
+	$GLOBALS['cfg']['instagram_push_endpoint'] = 'https://api.instagram.com/v1/subscriptions/';
+
 	#################################################################
 
 	function instagram_push_topic_map($string_keys=0){
@@ -16,6 +18,30 @@
 		}
 
 		return $map;
+	}
+
+	#################################################################
+
+	function instagram_push_subscribe(&$subscription){
+
+		$type_map = instagram_push_type_map();
+		$object = $type_map[$subscription['type_id']];
+
+		$callback = "$GLOBALS['cfg']['asb_root_url']}push/{$subscription['secret_url']}/";
+
+		$params = array(
+			'client_id' => $GLOBALS['cfg']['instagram_oauth_key'],
+			'client_secret' => $GLOBALS['cfg']['instagram_oauth_secret'],
+			'object' => $object,
+			'aspect' => 'media',
+			'verify_token' => $subscription['verify_token'],
+			'callback_url' => $callback,
+		);
+
+		$url = $GLOBALS['instagram_push_endpoint'];
+
+		$ret = http_post($url, $params);
+		return $ret;
 	}
 
 	#################################################################
