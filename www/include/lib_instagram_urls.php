012905dd341695da5047846702470ad51844c563
straup <straup@gmail.com>
2012-06-13 08:47:53 +0200

    sort out static paths

--- www/include/lib_instagram_urls.php
+++ www/include/lib_instagram_urls.php
@@ -10,7 +10,7 @@
 		# This is a *total hack* until I can work out how to
 		# fix this in flamework proper (20120321/straup)
 
-		$photo_root = $GLOBALS['cfg']['abs_root_url'] . "static/";
+		$photo_root = $GLOBALS['cfg']['abs_root_url'] . $GLOBALS['cfg']['instagram_static_url'];
 
 		$photo_path = storage_id_to_path($photo['id']);
 		$photo_fname = "{$photo['id']}_{$photo['secret']}.jpg";
